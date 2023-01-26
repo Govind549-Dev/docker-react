1157c033288ce4b5c24a87bd36e1b8da682a401d
Govinda549-Dev <govinda.mesala01@gmail.com>
2023-01-26 19:49:03 +0530

    Updated some script files

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          There is docker training is going on...!
+          Learn React
         </a>
       </header>
     </div>
